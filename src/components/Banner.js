13591d8b93d93d940e7b08d7c51d0e2231e1e96d
Charan Singh <charansingh25082002@gmial.com>
2024-04-13 16:00:46 +0530

    Banner.js all done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Web Developer", "Android Developer", "CyberSecurity Enthusiast" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,7 +56,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Charan`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Android Developer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`Hi! I'm Charan`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Android Developer", "CyberSecurity Enthusiast" ]'><span className="wrap">{text}</span></span></h1>
                   <p>Passionate about developing tech spaces and digital solutions and venturing deeper in the Tech world everyday.</p>
                   <button onClick={() => console.log('connect')}>Connect <ArrowRightCircle size={25} /></button>
               </div>}
