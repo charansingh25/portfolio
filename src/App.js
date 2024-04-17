ae8a1862f86de7abf4e914257561bf19b860b580
Charan Singh <charansingh25082002@gmial.com>
2024-04-17 20:03:18 +0530

    contact.js added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
-// import { Banner } from "./components/Banner";
-// import { Skills } from "./components/Skills";
-// import { Projects } from "./components/Projects";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 // import { Contact } from "./components/Contact";
 // import { Footer } from "./components/Footer";
 
@@ -12,11 +12,11 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* <Banner />
+      <Banner />
       <Skills />
       <Projects />
-      <Contact />
-      <Footer /> */}
+      {/* <Contact />
+      <Footer />  */}
     </div>
   );
 }
