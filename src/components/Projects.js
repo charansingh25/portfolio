cef70635f5b44194b567a7265ad55e6d9b5a5fe7
Charan Singh <charansingh25082002@gmial.com>
2024-04-24 01:43:34 +0530

    added few meter svg and changed some content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,7 +51,8 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                {/* <br></br> */}
+                <p>Explore a portfolio of diverse projects </p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
@@ -60,12 +61,13 @@ export const Projects = () => {
                     <Nav.Item>
                       <Nav.Link eventKey="second">Tab 2</Nav.Link>
                     </Nav.Item>
-                    <Nav.Item>
+                    {/* <Nav.Item>
                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
+                    </Nav.Item> */}
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
+                    <p>Web Development (MERN) | Android Development | Operating System </p>
                       <Row>
                         {
                           projects.map((project, index) => {
@@ -79,12 +81,24 @@ export const Projects = () => {
                         }
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Tab.Pane eventKey="second">
+                      <p>Discover my Sensors & IoT projects, where innovation meets practical application, demonstrating my ability to integrate smart technology into real-world solutions</p>
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
+                    {/* <Tab.Pane eventKey="third">
+                      <p>hello</p>
+                    </Tab.Pane> */}
                   </Tab.Content>
                 </Tab.Container>
               </div>}
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,16 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import meter60 from "../assets/img/meter60.svg";
+import meter50 from "../assets/img/meter50.svg";
+import meter65 from "../assets/img/meter65.svg";
+import meter70 from "../assets/img/meter70.svg";
+import meter75 from "../assets/img/meter75.svg";
+import meter80 from "../assets/img/meter80.svg";
+import meter85 from "../assets/img/meter85.svg";
+import meter90 from "../assets/img/meter90.svg";
+import meter95 from "../assets/img/meter95.svg";
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -35,58 +45,58 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>Versatile skill set spanning Web Development, Android Development, <br></br>Data Science, Cybersecurity,dedicated to leveraging technology to solve <br></br>complex problems and deliver innovative solutions</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter85} alt="Image" />
                                 <h5>C/C++</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter90} alt="Image" />
                                 <h5>Python</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter80} alt="Image" />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter75} alt="Image" />
                                 <h5>MERN</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter60} alt="Image" />
                                 <h5>CyberSecurity</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter70} alt="Image" />
                                 <h5>Flutter</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter75} alt="Image" />
                                 <h5>Dart</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter75} alt="Image" />
                                 <h5>Kotlin</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter70} alt="Image" />
                                 <h5>Blockchain</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter80} alt="Image" />
                                 <h5>Machine Learning</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter80} alt="Image" />
                                 <h5>MatLab</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter80} alt="Image" />
                                 <h5>R programming</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter80} alt="Image" />
                                 <h5>Numpy/Pandas</h5>
                             </div>
                             {/* <div className="item">
