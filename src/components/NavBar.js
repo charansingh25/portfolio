33b0dc3cd7abf0a23a23fbcc2c88264aaf5009e6
Charan Singh <charansingh25082002@gmial.com>
2024-04-24 03:13:45 +0530

    Logo size automating

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,7 +40,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" className="navbar-logo"/>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
