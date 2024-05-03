0a007a77117ffafaeb628c9c13d88cb22149ffb7
Charan Singh <charansingh25082002@gmial.com>
2024-05-03 22:43:15 +0530

    all done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,15 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+
+import interlinkd from "../assets/projects/interlinkd.png";
+import lumnio from "../assets/projects/lumino.png";
+import minespace from "../assets/projects/minespace.png";
+import osproject from "../assets/projects/osproject.png"
+import portfolio from "../assets/projects/portfolio.png";
+import seventyfive from "../assets/projects/seventyfive.png";
+
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,34 +20,34 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "InterLinkd",
+      description: "Messaging and networking platform",
+      imgUrl: interlinkd,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Lumnio",
+      description: "Digital Clock",
+      imgUrl: lumnio,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "Minespace",
+      description: "Parallax Login Website",
+      imgUrl: minespace,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Portfolio",
+      description: "Portfolio",
+      imgUrl: portfolio,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "SeventyFive",
+      description: "An Attendance App",
+      imgUrl: seventyfive,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
+      title: "Intelligent Process Termination",
+      description: "Operating System Project",
+      imgUrl: osproject,
     },
   ];
 
